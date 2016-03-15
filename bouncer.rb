83eb15e47bd5d0bd4add9aef32bff548661eda37
JaredPowers <jarpow44@gmail.com>
2016-03-15 17:53:15 -0500

    revisions

--- bouncer.rb
+++ bouncer.rb
@@ -1,31 +1,61 @@
-  def get_response(message)
-    puts message
-    print ">"
-    gets.chomp
+class Game
+  def start
+    puts ""
+    puts ""
+    puts "bouncer.rb v1.0345"
+    puts "-" * 20
+    sleep(0.8)
+    puts "---Welcome to the age identifier!!---"
+    puts "-----any key to start or (q)uit?-----"
+    @play = gets.chomp
+
+      while @play != 'q' do
+        print "How old are you?"
+        print ">"
+        @choice = gets.chomp.to_i
+        if @choice >= 1 && @choice < 18
+          puts "You can do nothing"
+          sleep (0.5)
+          puts "restarting...."
+          sleep (0.5)
+          Game.new.start
+        elsif @choice >= 18 && @choice < 21
+          puts "You can vote"
+          puts "You can smoke"
+          sleep (0.5)
+          puts "restarting...."
+          sleep (0.5)
+          Game.new.start
+        elsif @choice >= 21 && @choice < 25
+          puts "You can drink"
+          puts "You can vote"
+          puts "You can smoke"
+          sleep (0.5)
+          puts "restarting...."
+          sleep (0.5)
+          Game.new.start
+        elsif @choice > 25
+          puts "You can rent a car"
+          puts "You can drink"
+          puts "You can vote"
+          puts "You can smoke"
+          sleep (0.5)
+          puts "restarting...."
+          sleep (0.5)
+          Game.new.start
+        elsif @choice.to_s == "q"
+          puts "See ya later!"
+          exit
+        else
+          puts "Invalid answer"
+          puts "Do you not know your own age?"
+          sleep (0.5)
+          puts "restarting...."
+          sleep (0.5)
+          Game.new.start
+        end
+      end
   end
+end
 
-  puts "bouncer.rb v1.0345"
-  puts "-" * 20
-
-  choice = get_response ("Hello, How old are you? (Press 'Q' to quit)")
-
-  while choice != "q" do
-  choice = get_response ("Hello, How old are you? (Press 'Q' to quit)")
-
-  if choice.to_i >=1 && choice.to_i <18
-    puts "You can do nothing"
-  end
-
-  if choice.to_i >=18 && choice.to_i <21
-    puts "You can vote"
-    puts "You can smoke"
-  end
-
-  if choice.to_i >=21 && choice.to_i <25
-    puts "You can drink"
-  end
-
-  if choice.to_i >25
-    puts "You can rent a car"
-  end
-  end
+Game.new.start
